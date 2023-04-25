e042e33b2cd06533435742465c021f96d19d2e27
Galvin <77013913+GalvinPython@users.noreply.github.com>
2023-04-25 21:41:29 +0100

    Initial release

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
